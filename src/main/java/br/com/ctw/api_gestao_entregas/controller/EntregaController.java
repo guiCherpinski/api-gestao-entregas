758f1836cc37810807986c22d293d115f0c5aa69
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-23 16:46:40 -0300

    feat: Implementa EntregaController e EntregaService

--- src/main/java/br/com/ctw/api_gestao_entregas/controller/EntregaController.java
+++ src/main/java/br/com/ctw/api_gestao_entregas/controller/EntregaController.java
@@ -1,6 +1,7 @@
 package br.com.ctw.api_gestao_entregas.controller;
 
 import br.com.ctw.api_gestao_entregas.dto.EntregaResponseDTO;
+import br.com.ctw.api_gestao_entregas.service.EntregaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,14 +9,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/entregas")
 public class EntregaController {
 
+    private final EntregaService service;
+
+    public EntregaController(EntregaService service){
+        this.service = service;
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<EntregaResponseDTO> buscarEntrega(@PathVariable Long id){
-
+        return ResponseEntity.ok(service.buscarEntrega(id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarEntrega(@PathVariable Long id){
-
+        service.deletarEntrega(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/com/ctw/api_gestao_entregas/service/EntregaService.java
+++ src/main/java/br/com/ctw/api_gestao_entregas/service/EntregaService.java
@@ -1,6 +1,8 @@
 package br.com.ctw.api_gestao_entregas.service;
 
 import br.com.ctw.api_gestao_entregas.dto.EntregaResponseDTO;
+import br.com.ctw.api_gestao_entregas.entity.EntregaEntity;
+import br.com.ctw.api_gestao_entregas.mapper.EntregaMapper;
 import br.com.ctw.api_gestao_entregas.repository.EntregaRepository;
 import br.com.ctw.api_gestao_entregas.repository.MotoristaRepository;
 import jakarta.transaction.Transactional;
@@ -11,19 +13,23 @@ public class EntregaService {
 
     private final EntregaRepository repository;
     private final MotoristaRepository motoristaRepository;
+    private final EntregaMapper mapper;
 
-    public EntregaService(EntregaRepository repository, MotoristaRepository motoristaRepository){
+    public EntregaService(EntregaRepository repository, MotoristaRepository motoristaRepository, EntregaMapper mapper){
         this.repository = repository;
         this.motoristaRepository = motoristaRepository;
+        this.mapper = mapper;
     }
 
     @Transactional
     public EntregaResponseDTO buscarEntrega(Long id){
-
+        EntregaEntity entity = repository.findById(id).orElseThrow(() -> new RuntimeException("erro - entrega não encontrada"));
+        EntregaResponseDTO response = mapper.toResponse(entity);
+        return response;
     }
 
     @Transactional
     public void deletarEntrega(Long id){
-
+        repository.deleteById(id);
     }
 }

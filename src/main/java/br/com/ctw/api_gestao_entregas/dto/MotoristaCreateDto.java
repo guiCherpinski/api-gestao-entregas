a1b3f4aec18ba0d1dfdc0c7780ccbd50786550d0
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-23 15:41:49 -0300

    feat: Cria dtos para motoristas

---
+++ src/main/java/br/com/ctw/api_gestao_entregas/dto/MotoristaCreateDto.java
@@ -0,0 +1,7 @@
+package br.com.ctw.api_gestao_entregas.dto;
+
+public record MotoristaCreateDto(
+        String nome,
+        String cnh
+) {
+}
---
+++ src/main/java/br/com/ctw/api_gestao_entregas/dto/MotoristaResponseDTO.java
@@ -0,0 +1,8 @@
+package br.com.ctw.api_gestao_entregas.dto;
+
+public record MotoristaResponseDTO(
+        Long id,
+        String nome,
+        String cnh
+) {
+}

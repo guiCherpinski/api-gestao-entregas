eb014b9686503da3c4d0e5105e1635ef0b189f43
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-23 16:00:05 -0300

    Implementa Service e Controller de motorista

--- src/main/java/br/com/ctw/api_gestao_entregas/controller/MotoristaController.java
+++ src/main/java/br/com/ctw/api_gestao_entregas/controller/MotoristaController.java
@@ -2,23 +2,32 @@ package br.com.ctw.api_gestao_entregas.controller;
 
 import br.com.ctw.api_gestao_entregas.dto.MotoristaCreateDto;
 import br.com.ctw.api_gestao_entregas.dto.MotoristaResponseDTO;
+import br.com.ctw.api_gestao_entregas.service.MotoristaService;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/motoristas")
 public class MotoristaController {
 
+    private final MotoristaService service;
+
+    public MotoristaController(MotoristaService service){
+        this.service = service;
+    }
+
     @PostMapping()
     public ResponseEntity<MotoristaResponseDTO> cadastrarMotorista(MotoristaCreateDto create){
-
+        return ResponseEntity.ok(service.cadastrarMotorista(create));
     }
 
     @GetMapping()
     public ResponseEntity<List<MotoristaResponseDTO>> listarMotoristas(){
-
+        return ResponseEntity.ok(service.listarMotoristas());
     }
 }
--- src/main/java/br/com/ctw/api_gestao_entregas/service/MotoristaService.java
+++ src/main/java/br/com/ctw/api_gestao_entregas/service/MotoristaService.java
@@ -2,6 +2,9 @@ package br.com.ctw.api_gestao_entregas.service;
 
 import br.com.ctw.api_gestao_entregas.dto.MotoristaCreateDto;
 import br.com.ctw.api_gestao_entregas.dto.MotoristaResponseDTO;
+import br.com.ctw.api_gestao_entregas.entity.MotoristaEntity;
+import br.com.ctw.api_gestao_entregas.mapper.MotoristaMapper;
+import br.com.ctw.api_gestao_entregas.repository.MotoristaRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import java.util.*;
@@ -9,13 +12,26 @@ import java.util.*;
 @Service
 public class MotoristaService {
 
+    private final MotoristaRepository repository;
+    private final MotoristaMapper mapper;
+
+    public MotoristaService(MotoristaRepository repository, MotoristaMapper mapper){
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
     @Transactional
     public MotoristaResponseDTO cadastrarMotorista(MotoristaCreateDto create){
-
+        MotoristaEntity entity = mapper.toEntity(create);
+        repository.save(entity);
+        MotoristaResponseDTO response = mapper.toResponse(entity);
+        return response;
     }
 
     @Transactional
     public List<MotoristaResponseDTO> listarMotoristas(){
-
+        List<MotoristaEntity> motoristas = repository.findAll();
+        List<MotoristaResponseDTO> responses = mapper.toResponseList(motoristas);
+        return responses;
     }
 }

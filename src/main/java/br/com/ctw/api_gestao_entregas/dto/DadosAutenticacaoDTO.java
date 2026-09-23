cd6f0767cf29953edab0e183a95b5ae8a5f31ae6
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-23 18:22:20 -0300

    feat: Cria DTOs para envio de dados e recepcao do token

---
+++ src/main/java/br/com/ctw/api_gestao_entregas/dto/DadosAutenticacaoDTO.java
@@ -0,0 +1,7 @@
+package br.com.ctw.api_gestao_entregas.dto;
+
+public record DadosAutenticacaoDTO(
+        String username,
+        String password
+) {
+}
---
+++ src/main/java/br/com/ctw/api_gestao_entregas/dto/DadosTokenJWT.java
@@ -0,0 +1,6 @@
+package br.com.ctw.api_gestao_entregas.dto;
+
+public record DadosTokenJWT(
+        String token
+) {
+}
